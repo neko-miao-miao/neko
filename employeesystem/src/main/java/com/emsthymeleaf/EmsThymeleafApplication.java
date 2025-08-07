fe1b0cf94a6f936d309b251998ec4166665ba76c
neko-miao-miao <2328574657@qq.com>
2025-08-07 14:36:56 +0800

    Update EmsThymeleafApplication.java

--- employeesystem/src/main/java/com/emsthymeleaf/EmsThymeleafApplication.java
+++ employeesystem/src/main/java/com/emsthymeleaf/EmsThymeleafApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmsThymeleafApplication {//localhost:8088/ems-thymeleaf/......
+public class EmsThymeleafApplication {//localhost:8088/ems-thymeleaf/login启动
 //运行前先修改application.properties中的图片存储绝对路径和mysql数据库的配置
 	public static void main(String[] args) {
 		SpringApplication.run(EmsThymeleafApplication.class, args);
